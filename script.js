2e6fd5ab3dae6e86ecff86d4d83418e826849b51
Erik Faye-Lund <erik.faye-lund@collabora.com>
2024-05-05 19:41:55 +0200

    remove stale debug-logging

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
   var bar = myNodelist[i].id;
-  console.log('bar: ' + bar);
   if (localStorage.getItem(bar) === '1')
     myNodelist[i].classList.toggle('checked');
 }
@@ -12,7 +11,6 @@ var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
-    console.log('bar: ' + ev.target.id);
     localStorage.setItem(ev.target.id, ev.target.classList.contains('checked') ? '1' : '0')
   }
 }, false);
